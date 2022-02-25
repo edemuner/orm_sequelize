4ad3f1b42d02a143717724dcc0f3d0598d20c96c
edemuner <eduardood15@hotmail.com>
2022-02-25 19:32:52 -0300

    created people controller

---
+++ api/controllers/PeopleController.js
@@ -0,0 +1,16 @@
+const database = require('../models')
+
+class PeopleController {
+
+    static async getAllPeople(req, res){
+
+        try{
+            const allPeople = await database.People.findAll()
+            return res.status(200).json(allPeople)
+        } catch(error){
+            return res.status(500).json(error)
+        }
+    }
+}
+
+module.exports = PeopleController
