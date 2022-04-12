59553a0d3a6a51dadf1203929a2ab22194980662
edemuner <eduardood15@hotmail.com>
2022-04-11 21:19:19 -0300

    made a test to update class

--- cypress/integration/api-tests/api.spec.js
+++ cypress/integration/api-tests/api.spec.js
@@ -1,4 +1,6 @@
+
 const NEWCLASS = {start_date: "2022-04-01", professor_id: 1,level_id: 1}
+const UPDATECLASS = {professor_id:2, level_id:2}
 
 describe('Classes routes testing', () => {
     
@@ -37,7 +39,7 @@ describe('Classes routes testing', () => {
                 method:'GET',
                 url:`/classes/${id}`
             }).should(res => {
-                 assert.isArray(res.body)
+                assert.isArray(res.body)
                 expect(res.status).to.eq(200)
                 expect(res.body[0]).to.have.all.keys('id', 'start_date', 'updatedAt', 'createdAt', 'deletedAt', 'professor_id', 'level_id')
                 expect(res.body[0].id).to.eq(id)
@@ -45,4 +47,26 @@ describe('Classes routes testing', () => {
             )
         })
     })
+
+    it('put /classes/:id should return correctly', () => {
+        cy.request({
+            method:'POST',
+            url:'/classes',
+            body:NEWCLASS
+        }).then(res => {
+            const id = res.body.id
+            cy.request({
+                method:'PUT',
+                url: `/classes/${id}`,
+                body:UPDATECLASS
+            }).should(res => {
+                expect(res.status).to.eq(200)
+                expect(res.body[0]).to.have.all.keys('id', 'start_date', 'updatedAt', 'createdAt', 'deletedAt', 'professor_id', 'level_id')
+                expect(res.body[0].professor_id).to.eq(UPDATECLASS.professor_id)
+                expect(res.body[0].level_id).to.eq(UPDATECLASS.level_id)
+            })
+        })
+    })
+
+
 })
